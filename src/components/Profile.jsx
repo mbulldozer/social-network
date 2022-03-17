c9c47c0b07799ef12c1a3105b96514cc57241b77
Aliaksandr Pauliukavets <al.pavlyukovets@yandex.by>
2022-03-17 21:37:23 +0300

    init: start social-network project

---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+const Profile = () => {
+    return (
+        <div className="profile">
+            <div>
+                <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt=""/>
+            </div>
+            <div>
+                Ava + desc
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
