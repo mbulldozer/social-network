7071a8d9b51c2f18df71dad0805a8465f1001809
Aliaksandr Pauliukavets <al.pavlyukovets@yandex.by>
2022-03-19 23:17:38 +0300

    feat: add Dialogs component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,36 @@
 import classes from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
+
+const DialogItem = (props) => {
+    const path = "/dialogs/";
+    return (
+        <div className={classes.dialog}>
+            <NavLink to={path + props.id}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+const Message = (props) => {
+    return (
+        <div className={classes.message}>{props.message}</div>
+    )
+}
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <DialogItem name="Petya" id='1' />
+                <DialogItem name="Yura" id='2' />
+                <DialogItem name="Olya" id='3' />
+                <DialogItem name="Andru" id='4' />
+                <DialogItem name="Blade" id='5' />
+            </div>
+            <div className={classes.messages}>
+                <Message message="Hi" />
+                <Message message="Where are you from?" />
+                <Message message="How old are you?" />
+            </div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,23 @@
 import classes from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
-            <div>
-                <a href="">Profile</a>
+            <div className={classes.item}>
+                <NavLink to="/profile" className={(navData) => navData.isActive ? classes.active : ""}>Profile</NavLink>
             </div>
-            <div>
-                <a href="">Messages</a>
+            <div className={classes.item}>
+                <NavLink to="/dialogs" className={(navData) => navData.isActive ? classes.active : ""}>Messages</NavLink>
             </div>
-            <div>
-                <a href="">News</a>
+            <div className={classes.item}>
+                <NavLink to="/news" className={(navData) => navData.isActive ? classes.active : ""}>News</NavLink>
             </div>
-            <div>
-                <a href="">Music</a>
+            <div className={classes.item}>
+                <NavLink to="/music" className={(navData) => navData.isActive ? classes.active : ""}>Music</NavLink>
             </div>
-            <div>
-                <a href="">Settings</a>
+            <div className={classes.item}>
+                <NavLink to="/settings" className={(navData) => navData.isActive ? classes.active : ""}>Settings</NavLink>
             </div>
         </nav>
     )
